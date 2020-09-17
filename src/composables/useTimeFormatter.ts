117b82366f360076a06dc45445daee2fe73d9a1b
fishu <pkaczoruk@wp.pl>
2020-09-17 18:31:42 +0200

    :hammer: Implement types

--- src/composables/useTimeFormatter.ts
+++ src/composables/useTimeFormatter.ts
@@ -1,18 +1,16 @@
 export default function useTimeFormatter() {
-  const getFormattedTime = (time, penalty = 0) => {
-    let total, min, s, ms, minFormat, sFormat, msFormat, penaltyFormat
-
+  const getFormattedTime = (time: number, penalty: number = 0) => { 
     if (penalty === -1) return 'dnf'
 
-    total = time + penalty
-    min = Math.floor(total / 60 / 1000)
-    s = Math.floor((total - min * 60 * 1000) / 1000)
-    ms = Math.floor((total % 1000) / 10)
+    const total = time + penalty
+    const min = Math.floor(total / 60 / 1000)
+    const s = Math.floor((total - min * 60 * 1000) / 1000)
+    const ms = Math.floor((total % 1000) / 10)
 
-    minFormat = min < 1 ? '' : `${min}:`
-    sFormat = (min > 0 && s < 10) ? `0${s}.` : `${s}.`
-    msFormat = ms < 10 ? `0${ms}` : `${ms}`
-    penaltyFormat = penalty === 2000 ? '+' : ''
+    const minFormat = min < 1 ? '' : `${min}:`
+    const sFormat = (min > 0 && s < 10) ? `0${s}.` : `${s}.`
+    const msFormat = ms < 10 ? `0${ms}` : `${ms}`
+    const penaltyFormat = penalty === 2000 ? '+' : ''
 
     return minFormat + sFormat + msFormat + penaltyFormat
   }
